4b7c7d9abcb6dddc086c3217088b50dddf739eda
Anshul Saha <anshulsaha15@gmail.com>
2020-07-05 22:50:42 +0530

    basic file structure

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def register(request):
+    return render(request, 'accounts/register.html')
--- encryptid/settings.py
+++ encryptid/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'encryptid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'encryptid/static')
+]
