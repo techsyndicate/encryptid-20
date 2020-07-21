d5884608bff6e7efb72a97dac9e1ef6c85d5d1c8
Anshul Saha <anshulsaha15@gmail.com>
2020-07-21 20:08:48 +0530

    made the users part of the adminarea

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('login', views.login, name="login"),
     path('register', views.register, name="register"),
     path('play', views.dashboard, name="dashboard"),
+    path('admin_dashboard', views.admin_dashboard, name="admin_dashboard"),
     path('logout', views.logout, name="logout"),
     path('country/<str:code>/',views.level_call,name="level_call"),
+    path('admin_dashboard/users',views.users,name="users"),
+    path('admin_dashboard/users/user',views.user,name="user"),
+    path('admin_dashboard/users/user/delete',views.delete_user,name="delete_user"),
+    path('admin_dashboard/users/user/ban',views.ban_user,name="ban_user"),
+    path('admin_dashboard/users/user/unban',views.unban_user,name="unban_user"),
 ]
