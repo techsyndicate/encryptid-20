197ee6e9d9f2803dc243b31d5a4bd4492f2dc6a9
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2020-07-21 19:19:26 +0530

    Fetch levels + refactor map and banned page (#2)

--- pages/views.py
+++ pages/views.py
@@ -34,6 +34,7 @@ def register(request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+        name = f"{first_name} {last_name}"
 
         if User.objects.filter(username=username).exists():
             messages.error(request, 'That username is taken')
@@ -58,13 +59,15 @@ def register(request):
 
                     db.collection(u'users').document(username).set({
                         u'uid': username,
-                        u'name':first_name,
+                        u'name': name,
                         u'current_level': 0,
                         u'last_answer_time':0,
                         u'user_points': 0,
                         u'superuser': False,
                         u'banned': False,
                         u'email': email,
+                        u'completed_levels': [],
+                        u'current_level': ''
                     })
 
                     messages.success(request, 'You are now registered and can log in')
@@ -82,11 +85,16 @@ def dashboard(request):
     current_user = User.objects.get(id=request.user.id)
     username = current_user.username
     user = db.collection(u'users').document(username).get().to_dict()
+    completed_levels = len(user['completed_levels'])
+
     if user['banned']:
-        return render(request, 'pages/why-am-i-banned.html')
+        return redirect('banned')
     else:
-        return render(request, 'pages/dashboard.html')
-    
+        context = {
+            'username': username,
+            'completed_levels': completed_levels
+        }
+        return render(request, 'pages/dashboard.html', context)
 
 def logout(request):
     auth.logout(request)
@@ -94,7 +102,43 @@ def logout(request):
     
     return redirect('index')
 
-def level_call(request, code):
-    print(code)
-    return redirect('dashboard')
+@login_required(login_url='login')
+def play(request, code):
+    current_user = User.objects.get(id=request.user.id)
+    username = current_user.username
+    user_doc = db.collection(u'users').document(username)
+    user = user_doc.get().to_dict()
 
+    if user['banned']:
+        return redirect('banned')
+    else:
+        if code != user['current_level']:
+            code = user['current_level']
+            return redirect('play', code=code)
+        else:
+            user_doc.update({u'current_level': code})
+            level = db.collection(u'levels').document(code).get().to_dict()
+            question = level['question']
+            points = level['points']
+            src_hint = level['src_hint']
+            answer = level['answer']
+        
+        context = {
+            'id': code,
+            'question': question,
+            'points': points,
+            'hint': src_hint
+        }
+
+        return render(request, 'pages/level.html', context)
+
+@login_required(login_url='login')
+def banned(request):
+    current_user = User.objects.get(id=request.user.id)
+    username = current_user.username
+    user = db.collection(u'users').document(username).get().to_dict()
+
+    if not user['banned']:
+        return redirect('dashboard')
+
+    return render(request, 'pages/why-am-i-banned.html')
