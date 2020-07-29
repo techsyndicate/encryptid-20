bd35a856a5067a1090124a9c3b2a438f186379d4
Anshul Saha <anshulsaha15@gmail.com>
2020-07-29 17:58:38 +0530

    commented out urls and removed href of assign_duels button

--- pages/urls.py
+++ pages/urls.py
@@ -6,13 +6,7 @@ urlpatterns = [
     path('dashboard', views.dashboard, name="dashboard"),
     path('leaderboard', views.leaderboard, name="leaderboard"),
     path('why-am-i-banned', views.banned, name="banned"),
-    path('play/<str:code>', views.play, name="play"),
-    path('wait-for-next-duel', views.waiting_page, name="waiting_page"),
-
-    # path('play/<str:code>/submit', play.submit, name="submit"),
-    # path('play/<str:code>/skip', play.skip_level, name="skip_level"),
-    # path('duel', play.play_duel, name="play_duel"),
-
+    
     path('login', auth.login, name="login"),
     path('register', auth.register, name="register"),
     path('logout', auth.logout, name="logout"),
@@ -31,5 +25,12 @@ urlpatterns = [
     path('admin/levels/duel_level', admin.duel_level, name="duel_level"),
     path('admin/levels/add_duel_level', admin.add_duel_level, name="add_duel_level"),
     path('admin/levels/duel_level/delete' ,admin.delete_duel_level, name="delete_duel_level"),
-    path('admin/assign-duels', admin.assign_duels, name="assign_duels")
+    
 ]
+
+# path('play/<str:code>/submit', play.submit, name="submit"),
+# path('play/<str:code>/skip', play.skip_level, name="skip_level"),
+# path('duel', play.play_duel, name="play_duel"),
+# path('play/<str:code>', views.play, name="play"),
+# path('admin/assign-duels', admin.assign_duels, name="assign_duels"),
+# path('wait-for-next-duel', views.waiting_page, name="waiting_page"),
